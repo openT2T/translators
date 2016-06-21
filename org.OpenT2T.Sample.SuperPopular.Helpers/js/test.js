01755facf763fcb884f7cb8e8f1f48f6bca11f0d
rlingineni <Raviteja@Ravitejas-MBP-2.huadianz.me>
2016-06-20 23:27:49 -0700

    updated test.js to be readable

--- org.OpenT2T.Sample.SuperPopular.Helpers/js/test.js
+++ org.OpenT2T.Sample.SuperPopular.Helpers/js/test.js
@@ -4,13 +4,21 @@ var wh = require('./index');
 
 wh.initWinkApi("endpointHere","deviceID","accesstoken");
 
- wh.sendDesiredStateCommand('apiField',value).then(result => {
-   console.log(result);}).catch(error => {console.log(error.message);}); //for logging
+wh.sendDesiredStateCommand('apiField', value).then(result => {
+   console.log(result);
+}).catch (error => {
+    console.log(error.message);
+}); //for logging
 
 wh.getLastReading('apiFieldtoRead').then(result => {
-   console.log(result);}).catch(error => {console.log(error.message);});
+   console.log(result);
+}).catch (error => {
+    console.log(error.message);
+}); //for logging
 
 wh.getValueOfDesiredState('apiFieldtoRead').then(result => {
-console.log(result);}).catch(error => {console.log(error.message);});
-
+   console.log(result);
+}).catch (error => {
+    console.log(error.message);
+}); //for logging
 
