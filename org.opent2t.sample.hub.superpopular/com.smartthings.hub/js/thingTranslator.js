9bca86b913c506be70ec1331d6a346fa7a71aed8
Mary Chau <marcha@microsoft.com>
2016-10-17 16:59:56 +0800

    Enable device udpate.

--- org.opent2t.sample.hub.superpopular/com.smartthings.hub/js/thingTranslator.js
+++ org.opent2t.sample.hub.superpopular/com.smartthings.hub/js/thingTranslator.js
@@ -15,6 +15,7 @@ class Translator {
         this._accessToken = accessTokenInfo;
         this._baseUrl = '';
         this._devicesPath = '/devices';
+        this._updatePath = '/update';
         this._name = "SmartThings hub"; // TODO: Can be pulled from OpenT2T global constants. This information is not available, at least, on hub hub.
     }
 
@@ -22,16 +23,42 @@ class Translator {
      * Get the list of devices discovered through the hub.
      */
     getHubResURI() {
-        if (this._baseUrl == '') {
-            var endpointPromise = this._getEndpoint();
+        
+        return this._hasValidEndpoint().then((isValid) => {
+            if(isValid == false) return undefined;
+            
+            var toReturn = {};
+            var filteredDevices = [];
 
-            return Promise.resolve(endpointPromise).then((endpointURI) => {
-                this._baseUrl = endpointURI;
-                return this._getAllDevices();
-            });
-        } else {
-            return this._getAllDevices();
-        }
+            return this._makeRequest(this._devicesPath, 'GET')
+                .then((devices) => {
+
+                    for (var i = 0; i < devices.length ; i++) {
+                        var smartThingsDevice = devices[i];
+                        
+                        // get the opent2t schema and translator for the SmartThings device
+                        var opent2tInfo = this._getOpent2tInfo(smartThingsDevice.deviceType);
+
+                        if (opent2tInfo != undefined) // we support the device                    
+                        {
+                            // we only need to return certain properties back
+                            var device = {};
+                            device.name = smartThingsDevice.name;
+
+                            // set the specific device object id to be the id
+                            device.id = smartThingsDevice.id;
+
+                            // set the opent2t info for the hue device
+                            device.openT2T = opent2tInfo;
+
+                            filteredDevices.push(device);
+                        }
+                    }
+
+                    toReturn.devices = filteredDevices;
+                    return toReturn;
+                });
+        });
     }
 
     /**
@@ -44,42 +71,48 @@ class Translator {
     /**
      * Gets device details (all fields), response formatted per http://www.developers.meethue.com/documentation/lights-api
      */
-    getDeviceDetailsAsync(deviceType, deviceId) {
+    getDeviceDetailsAsync(deviceId) {
 
-        return this._getEndpointList().then((endpointList) => {
-
-            var toReturn = {};
-            var filteredDevices = [];
-            var promises = [];
-            for (var endpoint in endpointLists) {
-                var endpointQuery = this._makeRequest(endpoint + this._devicesPath, 'GET');
-                promises.push(endpointQuery);
-            }
-
-            return Promise.all(promises).then((responses) => {
-                console.loge(responses);
-                return responses;
-            });
+        return this._hasValidEndpoint().then((isValid) => {
+            if(isValid == false) return undefined;
+            
+            return this._makeRequest(this._devicesPath + '/' + deviceId , 'GET')
+                .then((device) => {
+                    return device;
+                });
         });
     }
 
     /**
      * Puts device details (all fields) payload formatted per http://www.developers.meethue.com/documentation/lights-api
-    */
-    putDeviceDetailsAsync(deviceType, deviceId, putPayload) {
-
-        return '';
+     */
+    putDeviceDetailsAsync(deviceId, putPayload) {
+        return this._hasValidEndpoint().then((isValid) => {
+            if(isValid == false) return undefined;
+            
+            var putPayloadString = JSON.stringify(putPayload);
+            console.log("Updateing device:")
+            console.log(putPayloadString);
+            return this._makeRequest(this._updatePath + '/' + deviceId, 'PUT', putPayloadString)
+                .then((result) => {
+                    if(result === "succeed")
+                    {
+                        return this.getDeviceDetailsAsync(deviceId);
+                    }
+                    return undefined;
+                });
+        });
     }
 
     /** 
      * Given the hub specific device, returns the opent2t schema and translator
-    */
+     */
     _getOpent2tInfo(deviceType) {
         switch(deviceType){
             case "light":
                 return { 
                     "schema": 'org.opent2t.sample.lamp.superpopular',
-                    "translator": "opent2t-translator-com-hue-bulb"
+                    "translator": 'opent2t-translator-com-smartthings-lightbulb'
                 };
             default:
                 return undefined;
@@ -89,8 +122,7 @@ class Translator {
     /**
      * Get the endpoint URI associated to the account
      */
-    _getEndpoint()
-    {
+    _getEndpoint() {
         var endpointUrl = 'https://graph.api.smartthings.com/api/smartapps/endpoints/' + this._accessToken.clientId + '?access_token=' + this._accessToken.accessToken;
 
         return this._makeRequest(endpointUrl, 'GET').then((responses) => {
@@ -104,41 +136,18 @@ class Translator {
     /**
      * Get all OpenT2T-supported devices from the based (endpoint) URI
      */
-    _getAllDevices() {
-        var toReturn = {};
-        var filteredDevices = [];
-
-        return this._makeRequest(this._devicesPath, 'GET')
-            .then((devices) => {
-
-                for (var i = 0; i < devices.length ; i++) {
-                    var smartThingsDevice = devices[i];
-
-                    // get the opent2t schema and translator for the SmartThings device
-                    var opent2tInfo = this._getOpent2tInfo(smartThingsDevice.deviceType);
-
-                    if (opent2tInfo != undefined) // we support the device                    
-                    {
-                        // we only need to return certain properties back
-                        var device = {};
-                        device.name = smartThingsDevice.name;
-
-                        // set the specific device object id to be the id
-                        device.id = smartThingsDevice.id;
-
-                        // set the opent2t info for the hue device
-                        device.openT2T = opent2tInfo;
-
-                        filteredDevices.push(device);
-                    }
-                }
-
-                toReturn.devices = filteredDevices;
-                return toReturn;
+    _hasValidEndpoint() {
+        if (this._baseUrl === '') {
+            return this._getEndpoint().then((endpointURI) => {
+                if(endpointURI === undefined) return Promise.resolve(false);
+                this._baseUrl = endpointURI;
+                return Promise.resolve(true)
             });
+        } else {
+            return Promise.resolve(true);
+        }
     }
-
-
+    
     /**
      * Internal helper method which makes the actual request to the hue service
      */
@@ -172,11 +181,16 @@ class Translator {
         // Start the async request
         return request(options)
             .then(function (body) {
+                if(method === 'PUT'){
+                    if(body.length === 0) return "succeed";
+                    return "Unkown error";
+                }
                 return JSON.parse(body);
             })
             .catch(function (err) {
                 console.log("Request failed to: " + options.method + " - " + options.url);
-                console.log("Error            : " + err.statusCode + " - " + err.response.statusMessage);
+                console.log("Error            : " + err);
+                //console.log("Error            : " + err.statusCode + " - " + err.response.statusMessage);
                 // todo auto refresh in specific cases, issue 74
                 throw err;
             });
