46c53e109269d42461263ffb5462db5b49e72347
Chuck Ferring <chuckfer@microsoft.com>
2016-10-25 16:10:29 -0700

    Added a gulp task to validate translator names.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,53 @@
+"use strict";
+
 var gulp = require('gulp');
 var xmlValidator = require('gulp-xml-validator');
 var eslint = require('gulp-eslint');
+var gutil = require('gulp-util');
+var through = require('through2');
+var path = require('path');
+
+var namingValidator = function() {
+  const PLUGIN_NAME = 'Naming Validator';
+  const NAME_PREFIX = 'opent2t-translator-';
+  var errorOutputList = [];
+
+  function transformFunction (file, encoding, callback) {
+    if (file.isNull()) {
+      return callback(null, file);
+    }
+
+    if (file.isStream()) {
+      return callback(new gutil.PluginError(PLUGIN_NAME, 'Streaming not supported'));
+    }
+
+    var metadata = JSON.parse(file.contents.toString());
+    var folderName = path.basename(path.dirname(path.dirname(file.path)));
+    var expectedName = NAME_PREFIX + folderName.replace(/\./g, '-');
+
+    if(metadata.name !== expectedName) {
+        errorOutputList.push(file.path + '\n' + gutil.colors.red("Error: ") + 'Incorrect translator name. Expected: ' + expectedName);
+    }
+
+    return callback(null, file);
+  }
+
+  function errorOutput(callback) {
+    if (errorOutputList.length > 0) {
+        this.emit('error', new gutil.PluginError(PLUGIN_NAME, '\n' + errorOutputList.join('\n\n') + '\n', {
+            showStack: false
+        }));
+     }
+     callback();
+  }
+
+  return through.obj(transformFunction, errorOutput);
+}
+
+gulp.task('validate-naming', function () {
+  return gulp.src(['**/js/package.json', '!**/node_modules/**', '!**/Helpers/**'])
+    .pipe(namingValidator())
+});
 
 gulp.task('xml-lint', function () {
   return gulp.src(['**/*.xml', '!**/node_modules/**'])
@@ -28,4 +75,4 @@ gulp.task('raml-lint', () => {
         .pipe(eslint.failAfterError());
 });
 
-gulp.task('ci-checks', ['js-lint', 'json-lint', 'xml-lint']);
+gulp.task('ci-checks', ['js-lint', 'json-lint', 'xml-lint', 'validate-naming']);
