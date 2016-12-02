60c162fa62376f72fece34aca54ace5d7c31c7fa
Mary Chau <marcha@microsoft.com>
2016-12-02 17:59:33 +0800

    Enable color control through colouRGB resource.

--- org.opent2t.sample.lamp.superpopular/com.smartthings.lightbulb/js/thingTranslator.js
+++ org.opent2t.sample.lamp.superpopular/com.smartthings.lightbulb/js/thingTranslator.js
@@ -31,23 +31,133 @@ function findResource(schema, di, resourceId) {
 }
 
 /**
+ * Colour Conversion Funcitons
+ */
+
+const ChangeTolerance = 0.0001;
+const MaxHue = 360.0;
+const MaxColor = 255;
+
+/**
+ * Convert HSV to RGB colours
+ *   0 <= Hue <= 360
+ *   0 <= Saturation <= 1
+ *   0 <= lumosity <= 1 
+ */
+function HSVtoRGB(hue, saturation, lumosity) {
+    var hi = Math.floor(hue / 60) % 6;
+    var c = saturation * lumosity;
+    var x = c * (1 - Math.abs(((hue / 60) % 2) - 1));
+    var m = lumosity - c;
+
+    var result;
+
+    switch (hi)
+    {
+        case 0:
+            result = [c, x, 0];
+            break;
+        case 2 :
+            result = [x, c, 0];
+            break;
+        case 3:
+            result = [0, c, x];
+            break;
+        case 4:
+            result = [0, x, c];
+            break;
+        case 5:
+            result = [x, 0, c];
+            break;
+        default:
+            result = [c, 0, x];
+            break;
+    }
+
+    return [(result[0] + m) * MaxColor, (result[1] + m) * MaxColor, (result[2] + m) * MaxColor]
+}
+
+/**
+ * Convert RGB to HSV colours
+ * RGB values are in [0 ... 255]
+ */
+function RGBtoHSV(rgbValue) {
+    var red = rgbValue[0] / MaxColor;
+    var green = rgbValue[1] / MaxColor;
+    var blue = rgbValue[2] / MaxColor;
+
+    var min = Math.min(red, green, blue);
+    var max = Math.max(red, green, blue);
+    var delta = max - min;
+
+    // fraction to byte
+    var hue = 0 , saturation = 0;
+
+    if (Math.abs(max) > ChangeTolerance)
+        saturation = delta/max;
+    else
+    {
+        // don't update Hue or saturation
+        return undefined;
+    }
+
+    if (Math.abs(delta) < ChangeTolerance)
+        return undefined;
+
+    if (Math.abs(red - max) < ChangeTolerance)
+        hue = (green - blue)/delta; // between yellow & magenta
+    else if (Math.abs(green - max) < ChangeTolerance)
+        hue = 2 + (blue - red)/delta; // between cyan & yellow
+    else
+        hue = 4 + (red - green)/delta; // between magenta & cyan
+
+    hue *= 60; // degrees
+    if (hue < 0)
+        hue += MaxHue;
+    if (hue > MaxHue)
+        hue -= MaxHue;
+
+    return {
+        'hue': hue,
+        'saturation': saturation,
+        'level': max
+    }
+}
+
+/**
  * Converts a representation of a platform from the Wink API into an OCF representation.
  */
 function providerSchemaToPlatformSchema(providerSchema, expand) {
 
-    // Build the oic.r.switch.binary resource
+    // Build the power resource
     var power = {
         "href": "/power",
         "rt": ["oic.r.switch.binary"],
         "if": ["oic.if.a", "oic.if.baseline"]
     }
 
-    // Build the oic.r.dimming resource
+    // Build the dimming resource
     var dim = {
         "href": "/dim",
         "rt": ["oic.r.light.dimming"],
         "if": ["oic.if.a", "oic.if.baseline"]
     }
+
+    // Build the colourMode resource
+    var colourMode = {
+        "href": "/colourMode",
+        "rt": ["oic.r.mode"],
+        "if": ["oic.if.s", "oic.if.baseline"]
+    }
+
+    // Build the colourRGB resource
+    var colourRGB = {
+        "href": "/colourRGB",
+        "rt": ["oic.r.colour.rgb"],
+        "if": ["oic.if.a", "oic.if.baseline"]
+    }
+
+    //TODO: ct = providerSchema['attributes'].colorTemperature
 
     // Include the values is expand is specified
     if (expand) {
@@ -57,6 +167,16 @@ function providerSchemaToPlatformSchema(providerSchema, expand) {
         dim.id = 'dim';
         dim.dimmingSetting = providerSchema['attributes'].level;
         dim.range = [0, 100];
+
+        colourMode.id = 'colourMode';
+        colourMode.modes = 'rgb';
+        colourMode.supportedModes = ['rgb', 'chroma'];
+
+        colourRGB.id = 'colourRGB';
+        colourRGB.rgbvalue = HSVtoRGB(providerSchema['attributes'].hue * MaxHue / 100.0,
+                                      providerSchema['attributes'].saturation / 100.0,
+                                      providerSchema['attributes'].level / 100.0);
+        colourRGB.range = [0, 255];
     }
 
     return {
@@ -78,7 +198,9 @@ function providerSchemaToPlatformSchema(providerSchema, expand) {
                 dmv: 'res.1.1.0',
                 resources: [
                     power,
-                    dim
+                    dim,
+                    colourMode,
+                    colourRGB
                 ]
             }
         ]
@@ -91,7 +213,7 @@ function providerSchemaToPlatformSchema(providerSchema, expand) {
 function resourceSchemaToProviderSchema(resourceId, resourceSchema) {
 
     // build the object with desired state
-    var result = {};  
+    var result = {};
 
     switch (resourceId) {
         case 'power':
@@ -100,8 +222,19 @@ function resourceSchemaToProviderSchema(resourceId, resourceSchema) {
         case 'dim':
             result['level'] = resourceSchema.dimmingSetting;
             break;
+        case 'colourRGB':
+            var HSVColor = RGBtoHSV(resourceSchema.rgbvalue);
+            console.log(HSVColor);
+            if (HSVColor !== undefined)
+            {
+                result['hue'] = Math.round(HSVColor.hue / MaxHue * 100);
+                result['saturation'] = Math.round(HSVColor.saturation * 100);
+                result['level'] = Math.round(HSVColor.level * 100);
+            }
+            break;
         default:
             // Error case
+            console.log("Invalid resourceId: " + resourceId)
             throw new Error("Invalid resourceId");
     }
     return result;
@@ -156,14 +289,18 @@ class Translator {
      * Updates the specified resource with the provided payload.
      */
     postDeviceResource(di, resourceId, payload) {
-        var putPayload = resourceSchemaToProviderSchema(resourceId, payload);
+        if (di === controlId) {
+            var putPayload = resourceSchemaToProviderSchema(resourceId, payload);
 
-        return smartThingsHub.putDeviceDetailsAsync(controlId, putPayload)
-            .then((response) => {
-                var schema = providerSchemaToPlatformSchema(response, true);
+            return smartThingsHub.putDeviceDetailsAsync(controlId, putPayload)
+                .then((response) => {
+                    var schema = providerSchemaToPlatformSchema(response, true);
 
-                return findResource(schema, di, resourceId);
-            });
+                    return findResource(schema, di, resourceId);
+                });
+        } else {
+            throw new Error('NotFound');
+        }
     }
 
     getDevicesPower(controlId) {
@@ -179,11 +316,11 @@ class Translator {
     }
 
     getDevicesColourRGB(controlId) {
-        return this.getDeviceResource(controlId, "colourRgb");
+        return this.getDeviceResource(controlId, "colourRGB");
     }
 
     postDevicesColourRGB(controlId, payload) {
-        return this.postDeviceResource(controlId, "colourRgb", payload);
+        return this.postDeviceResource(controlId, "colourRGB", payload);
     }
 
     getDevicesDim(controlId) {
