c7e3195f377c7b8dd8b022806a118c90aab24bb9
m-chau <marcha@MARY_DESKTOP>
2017-03-29 18:58:55 +0800

    Update test files.

--- org.opent2t.sample.multisensor.superpopular/com.smartthings.sensorpod/js/tests/unitTests.js
+++ org.opent2t.sample.multisensor.superpopular/com.smartthings.sensorpod/js/tests/unitTests.js
@@ -3,7 +3,7 @@ var translatorPath = require('path').join(__dirname, '..');
 var runMultisensorTests = require('opent2t-device-multisensor/multisensorTests');
 var deviceData = require('./devicedata');
 var testData = require('./testdata');
-var MockHub = require('opent2t-device-smartthingshub/mockSmartThingsHub');
+var MockHub = require('opent2t-device-smartthingshub/mockSmartthingsHub');
 var mockHub = new MockHub(deviceData);
 
 var settings = {
